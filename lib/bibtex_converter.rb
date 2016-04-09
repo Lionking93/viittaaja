3487105ea32ec26dbc730f79250bd48a443b769f
Valtteri Lakaniemi <valtteri.lakaniemi@gmail.com>
2016-04-09 13:39:28 +0300

    fixed bug with empty fields

--- lib/bibtex_converter.rb
+++ lib/bibtex_converter.rb
@@ -15,17 +15,17 @@ module BibtexConverter
     converted = []
 
     converted << "@Book{#{data.id},"
-    converted << "year = {#{data.year}}," unless data.year.nil?
-    converted << "publisher = {#{self.fix_special(data.publisher)}}," unless data.publisher.nil?
-    converted << "author = {#{self.fix_special(data.author)}}," unless data.author.nil?
-    converted << "title = {#{self.fix_special(data.title)}}," unless data.title.nil?
-    converted << "address = {#{self.fix_special(data.address)}}," unless data.address.nil?
-    converted << "pages = {#{self.fix_special(data.pages)}}," unless data.pages.nil?
-    converted << "volume = {#{data.volume}}," unless data.volume.nil?
-    converted << "edition = {#{self.fix_special(data.edition)}}," unless data.edition.nil?
-    converted << "month = {#{data.month}}," unless data.month.nil?
-    converted << "series = {#{data.series}}," unless data.series.nil?
-    converted << "note = {#{self.fix_special(data.note)}}," unless data.note.nil?
+    converted << "year = {#{data.year}}," unless data.year.nil? || data.year.empty?
+    converted << "publisher = {#{self.fix_special(data.publisher)}}," unless data.publisher.nil? || data.publisher.empty?
+    converted << "author = {#{self.fix_special(data.author)}}," unless data.author.nil? || data.author.empty?
+    converted << "title = {#{self.fix_special(data.title)}}," unless data.title.nil? || data.title.empty?
+    converted << "address = {#{self.fix_special(data.address)}}," unless data.address.nil? || data.address.empty?
+    converted << "pages = {#{self.fix_special(data.pages)}}," unless data.pages.nil? || data.pages.empty?
+    converted << "volume = {#{data.volume}}," unless data.volume.nil? || data.volume.empty?
+    converted << "edition = {#{self.fix_special(data.edition)}}," unless data.edition.nil? || data.edition.empty?
+    converted << "month = {#{data.month}}," unless data.month.nil? || data.month.empty?
+    converted << "series = {#{data.series}}," unless data.series.nil? || data.series.empty?
+    converted << "note = {#{self.fix_special(data.note)}}," unless data.note.nil? || data.note.empty?
     converted << "}"
 
     converted
