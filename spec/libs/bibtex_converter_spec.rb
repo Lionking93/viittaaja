156426768e6d68d02c5c811a675d9050ae75f16d
karvonen <antti.karvonen@helsinki.fi>
2016-04-10 15:24:57 +0300

    fix_special tests added

---
+++ spec/libs/bibtex_converter_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+
+RSpec.describe BibtexConverter, type: :lib do
+  let(:dummy_class) { Class.new { include BibtexConverter } }
+  it "Ää is converted" do
+    conv = dummy_class.new
+    str = "Ää"
+
+    conv.fix_special(str)
+
+    str.should eq("\\\"{A}\\\"{a}")
+  end
+
+  it "Öö is converted" do
+    conv = dummy_class.new
+    str = "Öö"
+
+    conv.fix_special(str)
+
+    str.should eq("\\\"{O}\\\"{o}")
+  end
+
+  it "Åå is converted" do
+    conv = dummy_class.new
+    str = "Åå"
+
+    conv.fix_special(str)
+
+    str.should eq("\\AA\\aa")
+  end
+
+end
+
