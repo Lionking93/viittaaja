ee8c231d892dfc01f3fa1fc5c06ec6714e410035
Valtteri Lakaniemi <valtteri.lakaniemi@gmail.com>
2016-04-11 16:23:09 +0300

    bibtex-testi

--- spec/features/referencelist_page_spec.rb
+++ spec/features/referencelist_page_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 describe 'Referencelist page' do
 
+  before :each do
+    @list = Referencelist.create name:"lista"
+  end
+
   it 'lists all current references' do
 
 
@@ -12,7 +16,19 @@ describe 'Referencelist page' do
   end
 
   it 'when bibtex-button is pressed, opens up a page showing references in bibtex format' do
-    
+    @list.references.create year:1990, author:"Barack Öbämå", title:"USA", publisher:"asd"
+
+    visit referencelist_path(@list)
+
+    click_link "Bibtex"
+
+    expect(page).to have_content "Bibtex for lista"
+    expect(page).to have_content "@Book{1,
+                                  year = {1990},
+                                  publisher = {asd},
+                                  author = {Barack \\\"{O}b\\\"{a}m\\aa},
+                                  title = {USA},
+                                  }"
   end
 
 end
