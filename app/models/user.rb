466898bfddce21f02ac54448cec57930e43f7887
kummitus <kummitus123@gmail.com>
2016-04-11 15:12:54 +0300

    Controller tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   has_many :references, through: :referencelists
   has_many :referencelists
 
+  validates :username, length: { minimum: 3, maximum: 15 }, uniqueness: true
+
   def to_s
     return self.username.to_s
   end
