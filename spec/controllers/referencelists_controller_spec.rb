c616cc7999be39571cc655844da16935308f8973
Leo Kallonen <leo.kallonen@gmail.com>
2016-04-14 00:45:47 +0300

    testit wörkii

--- spec/controllers/referencelists_controller_spec.rb
+++ spec/controllers/referencelists_controller_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe ReferencelistsController, type: :controller do
-
+	describe "DELETE #destroy" do
+    	it "destroys the requested referencelist" do
+      		referencelist = Referencelist.create! name:"Ismon lista"
+      		expect {
+        		delete :destroy, {:id => referencelist.to_param}
+      		}.to change(Referencelist, :count).by(-1)
+    	end
+    	it "redirects to the referncelists list" do
+      		referencelist = Referencelist.create! name:"Ismon lista"
+      		delete :destroy, {:id => referencelist.to_param}
+     		expect(response).to redirect_to(referencelists_url)
+    	end
+	end
+	describe "GET #index" do
+    	it "Referencelist.all gets all referencelists" do
+      		referencelist1 = Referencelist.create! name:"Lista1"
+      		referencelist2 = Referencelist.create! name:"Lista2"
+      		referencelist3 = Referencelist.create! name:"Lista3"
+      		get :index
+      		expect(assigns(:referencelists)).to eq(Referencelist.all)
+    	end
+  	end
+  	describe "GET #new" do
+    	it "getting new" do
+      		get :new
+      		expect(response.status).to eq(200)
+    	end
+  	end
+  	describe "POST #create" do
+    	it "creating referencelist with parameters" do
+      		expect{
+        	post :create, {:referencelist => {:name => "lista"}}
+      		}.to change(Referencelist, :count).by(1)
+    	end
+    	it "invalid referencelist creation not possible" do
+      		expect{
+        	post :create, {:referencelist => {:name => ""}}
+      		}.to change(Referencelist, :count).by(0)
+    	end
+  	end
+  	describe "PUT #update" do
+    	it "update success" do
+      		list1 = Referencelist.create! name:"Lista1"
+      		put :update, {:id => list1.to_param, :referencelist => {:name => "uusilista"} }
+      		list1.reload
+      		expect(list1.name).to eq("uusilista")
+    	end
+    	it "update fails" do
+      		list1 = Referencelist.create! name:"Lista1"
+      		put :update, {:id => list1.to_param, :referencelist => {:name => ""} }
+      		list1.reload
+      		expect(list1.name).to eq("Lista1")
+    	end
+  	end
 end
--- spec/features/referencelist_page_spec.rb
+++ spec/features/referencelist_page_spec.rb
@@ -7,12 +7,34 @@ describe 'Referencelist page' do
   end
 
   it 'lists all current references' do
+    @list.references.create year:1990, author:"Teppo", title:"Matti", publisher:"Suomiboyz"
+    @list.references.create year:1990, author:"Barack Öbämå", title:"USA", publisher:"asd"
+    visit referencelist_path(@list)
 
+    @first_reference = find('table tbody').find('tr:nth-child(1)')
+    @second_reference = find('table tbody').find('tr:nth-child(2)')
 
+    expect(@first_reference.text).to eq "1990 Suomiboyz Teppo Matti"
+    expect(@second_reference.text).to eq "1990 asd Barack Öbämå USA"
   end
 
   it 'when a new reference is added, shows it on the page' do
+    @list.references.create year:1990, author:"Teppo", title:"Matti", publisher:"Suomiboyz"
+    @list.references.create year:1990, author:"Barack Öbämå", title:"USA", publisher:"asd"
+    visit referencelist_path(@list)
+    expect(page). to have_xpath(".//tr", count: 3)
 
+    expect(page).not_to have_content("2000 Kebab on hyvää")
+
+    click_link "Add book"
+    fill_in 'reference_year', with: '2000'
+    fill_in 'reference_publisher', with: 'Kebab'
+    fill_in 'reference_author', with: 'on'
+    fill_in 'reference_title', with: 'hyvää'
+    click_button "Create Reference"
+    expect(page).to have_xpath(".//tr", count: 4)
+
+    expect(page).to have_content("2000 Kebab on hyvää")
   end
 
   it 'when bibtex-button is pressed, opens up a page showing references in bibtex format' do
