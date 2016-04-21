d819905249d41fcc6e069effa7a33f119beb03cd
Leo Kallonen <leo.kallonen@gmail.com>
2016-04-21 10:19:21 +0300

    ei tyhjää kuukautta bibtexiin

--- lib/bibtex_converter.rb
+++ lib/bibtex_converter.rb
@@ -25,7 +25,7 @@ module BibtexConverter
     converted << "pages = {#{self.fix_special(data.pages)}}," unless data.pages.nil? || data.pages.empty?
     converted << "volume = {#{data.volume}}," unless data.volume.nil?
     converted << "edition = {#{self.fix_special(data.edition)}}," unless data.edition.nil? || data.edition.empty?
-    converted << "month = {#{data.month}}," unless data.month.nil?
+    converted << "month = {#{data.month}}," unless data.month.nil? || data.month.empty?
     converted << "series = {#{data.series}}," unless data.series.nil?
     converted << "note = {#{self.fix_special(data.note)}}," unless data.note.nil? || data.note.empty?
     converted << "journal = {#{self.fix_special(data.journal)}}," unless data.journal.nil? || data.journal.empty?
