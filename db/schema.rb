16dcf2a06886991dcf9680a70b278f8a1a36f9c1
Leo Kallonen <leo.kallonen@gmail.com>
2016-04-24 14:24:13 +0300

    fix ehkä

--- db/schema.rb
+++ db/schema.rb
@@ -47,6 +47,12 @@ ActiveRecord::Schema.define(version: 20160424094816) do
 
   add_index "references", ["user_id"], name: "index_references_on_user_id"
 
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "references_tags", id: false, force: :cascade do |t|
     t.integer "reference_id"
     t.integer "tag_id"
@@ -54,12 +60,6 @@ ActiveRecord::Schema.define(version: 20160424094816) do
 
   add_index "references_tags", ["reference_id"], name: "index_references_tags_on_reference_id"
   add_index "references_tags", ["tag_id"], name: "index_references_tags_on_tag_id"
-
-  create_table "tags", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
