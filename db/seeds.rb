abf0e8e9d56a631ff8f74a9f374f11e9e1d492fe
kummitus <kummitus123@gmail.com>
2016-04-25 13:28:31 +0300

    All inclusive angular page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user = User.create username:"default"
-
-rl = Referencelist.create user:user, name:"default list"
-
-rl.references.create author:"Martin, Robert", title:"Clean code: a handbook of agile software craftmanship", year:2008, publisher:"Prentice Hall"
-rl.references.create year:2005, author:"Matti Meikäläinen", publisher:"Ötävå", title:"asdasdasdasdas", volume:2
-rl.references.create year:2028, author:"Maasddsa eikäläcsadsainen", publisher:"Ötävå", title:"PPDSOAODPPDSA"
+rl = User.create username:"default"
