c1d85f62225a0a5b77d266190f871046e25ff40c
kummitus <kummitus123@gmail.com>
2016-04-27 21:17:51 +0300

    fuzzy search

--- app/assets/javascripts/initializelist.js
+++ app/assets/javascripts/initializelist.js
@@ -3,8 +3,17 @@
  */
 $( window ).load(function(){
 
+    var fuzzyOptions = {
+        searchClass: "fuzzy-search",
+        location: 0,
+        distance: 100,
+        treshold: 0.4,
+            multisearch: true
+    };
+
     var options = {
-        valueNames: [ 'citation_key', 'reference_type', 'year', 'publisher', 'journal', 'author', 'editor', 'title', 'booktitle', 'address', 'pages', 'volume', 'edition', 'organization', 'key', 'month', 'series', 'note', 'tag' ]
+        valueNames: [ 'reference_type', 'citation_key', 'reference_type', 'year', 'publisher', 'journal', 'author', 'editor', 'title', 'booktitle', 'address', 'pages', 'volume', 'edition', 'organization', 'key', 'month', 'series', 'note', 'tag' ],
+        plugins: [ ListFuzzySearch() ]
     };
 
     var referenceList = new List('references', options);
