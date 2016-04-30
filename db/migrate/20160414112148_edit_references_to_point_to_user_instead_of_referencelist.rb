46765b19ce9dbcf83b577c6ec1b3a676b763b27f
Valtteri Lakaniemi <valtteri.lakaniemi@gmail.com>
2016-04-30 20:53:38 +0300

    äh, korjautuuko build?

--- db/migrate/20160414112148_edit_references_to_point_to_user_instead_of_referencelist.rb
+++
@@ -1,7 +0,0 @@
-class EditReferencesToPointToUserInsteadOfReferencelist < ActiveRecord::Migration
-  def change
-    remove_column :references, :referencelist_id, :integer
-    add_column :references, :user_id, :integer
-    add_index :references, :user_id
-  end
-end
--- db/migrate/20160414112931_add_default_user_id_to_current_references.rb
+++
@@ -1,9 +0,0 @@
-class AddDefaultUserIdToCurrentReferences < ActiveRecord::Migration
-  def change
-    user = User.first
-
-    Reference.all.each do |ref|
-      ref.update_attribute(:user, user)
-    end
-  end
-end
